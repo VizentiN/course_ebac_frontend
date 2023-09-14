3bbcac27e612e70e5ba35740f7f49c9f0b883420
Lucas Eduardo Vizentin <91027807+VizentiN@users.noreply.github.com>
2023-09-14 20:11:11 +0100

    First commit

---
+++ exercise.ts
@@ -0,0 +1,5 @@
+const multiplicacao = (num1: number, num2: number): number => num1 * num2
+const dizHi = (nome:string): string => `${nome} está dizendo Hi!`
+
+console.log(multiplicacao(5,5))
+console.log(dizHi('Lucas'))
